0e2eb1a30f51fd2e6bac1a8cc41f2e09521bd153
Jose Zafra <jazb18@gmail.com>
2017-04-16 13:00:27 -0500

    commit

---
+++ src/main/java/com/jazb/restEasy/MessageRestService.java
@@ -0,0 +1,18 @@
+package com.jazb.restEasy;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class MessageRestService {
+	
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg){
+		
+		String output = "Luvao Consulting dice :" + msg ; 
+		return Response.status(200).entity(output).build(); 
+	}
+}
